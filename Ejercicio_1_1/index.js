9da6acbf5515ac72268d432c31828387a29b68a2
luciasg <77329616+luciasg@users.noreply.github.com>
2021-01-28 01:43:59 +0000

    Ejercicio 1.1

---
+++ Ejercicio_1_1/index.js
@@ -0,0 +1,12 @@
+
+// base de 16*23 cm
+// altura de 13 cm
+
+const volumen=16*23*13;
+console.log('volumen',volumen + 'cm3');
+
+// A = area lateral (al) + 2 veces el area de la base (2ab)
+const areaLateral=23*13;
+const areaBase=16*23;
+const areaTotal=areaLateral+2*areaBase;
+console.log('areaTotal',areaTotal + 'cm2');
