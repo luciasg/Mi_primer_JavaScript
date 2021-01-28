d6438b790e99693a8a20fcf0973dace706c6c39c
luciasg <77329616+luciasg@users.noreply.github.com>
2021-01-28 13:09:13 +0000

    Ejercicio 1.8

---
+++ Ejercicio_1_8/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+const frase = prompt('Escribe una frase');
+
+function info(cadena) {
+  var resultado = `La cadena "${cadena}" está formada por `;
+  if (cadena === cadena.toUpperCase()) {
+    resultado += "solo mayúsculas";
+  } else if (cadena === cadena.toLowerCase()) {
+    resultado += "solo minúsculas";
+  } else {
+    resultado += "minúsculas y mayúsculas";
+  }
+  return resultado
+}
+
+alert(info(frase));
