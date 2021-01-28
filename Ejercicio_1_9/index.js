8fd22905c5126b9e04b13c736a94af48ca8f6cee
luciasg <77329616+luciasg@users.noreply.github.com>
2021-01-28 13:38:40 +0000

    Ejercicio 1.9

---
+++ Ejercicio_1_9/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const topics = ['JavaScript', 'Variables', 'funciones', 'condicionales', 'bucles'];
+
+const capitalizeTopics = topics.map(eachTopic => eachTopic.toUpperCase());
+capitalizeTopics.reverse();
+console.log('topics', topics);
+console.log('capitalizeTopics', capitalizeTopics);
